4ddf8c989c58b31b96389d218e434a0fb434fb56
shivam <shivamgit@pm.me>
2020-12-19 10:47:15 +0530

    Add list all items route
    Signed-off-by: shivam <shivamgit@pm.me>

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -3,6 +3,23 @@ import { prisma } from '../utils';
 
 const router = Router();
 
+router.get('/', async (req, res) => {
+  const item = await prisma.item
+    .findMany({
+      select: {
+        id: true,
+        desc: true,
+        title: true,
+        price: true,
+        noOfRatings: true,
+        totalScore: true
+      }
+    })
+    .catch((e) => res.status(500).send(e));
+  if (item) return res.json(item);
+  return res.send(404);
+});
+
 router.get('/:itemId', async (req, res) => {
   const item = await prisma.item
     .findUnique({
